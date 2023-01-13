ddf58fadefa883a472db2e406ed1adb6310fd595
dimitris2ks <dimitris2kss@gmail.com>
2023-01-13 19:35:14 +0200

    until 3.3.3(hearing threshold)

---
+++ Dksparse.py
@@ -0,0 +1,50 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def Dksparse(Kmax: int) ->np.ndarray:
+    
+    sparse = np.zeros([Kmax,Kmax])
+    
+    for k in range(2,int(0.24479167*Kmax)):
+        
+        sparse[k,k-2] = 1
+        sparse[k,k+2] = 1  
+
+    for k in range(int(0.24479167*Kmax),int(0.49479167*Kmax)):
+        for j in range(2,14):
+            sparse[k,k-j] = 1
+            sparse[k,k+j] = 1
+    
+    for k in range(int(0.49479167*Kmax),Kmax):
+        for j in range(2,28):
+            sparse[k,k-j] = 1
+            if k+j < Kmax:
+                sparse[k,k+j] = 1
+    
+    return sparse            
+
+
+# trial=Dksparse(1151)
+# fig=plt.figure()
+# ax=plt.axes(projection='3d')
+# # for k in range (1151):
+# #    Indexes=np.argwhere(trial[k,:]==1).flatten()
+# #    ax.scatter(np.ones(Indexes.shape[0])*k,Indexes)
+ 
+# print(trial.shape) 
+   
+# x = range(trial.shape[0])
+# y = range(trial.shape[1])  
+
+# X, Y = np.meshgrid(x,y) 
+   
+# ax.scatter3D(X , Y, trial, c=trial, cmap='viridis' )   
+# ax.view_init(elev=90, azim=0)   
+# plt.show()
+ 
+ 
+ 
+# logical = np.array([0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,]) 
+# print(logical)    
+# data = np.argwhere(logical==1).flatten()
+# print(data)    
---
+++ STinit.py
@@ -0,0 +1,37 @@
+import numpy as np
+from matplotlib import pyplot as plt
+import Dksparse
+import DCTpower
+
+def STinit(c: np.ndarray, D: np.ndarray) -> np.ndarray:
+    
+    
+    P = DCTpower.DCTpower(c)
+    ST = []
+    
+    for k in range(0,c.shape[0]):
+        
+        Dk = [P*D[k,:]]
+        Pk = P[k] - Dk - 7
+        count = np.count_nonzero(Pk < 0)
+        
+        if k < P.shape[0]-1:
+            if P[k] > P[k-1] and P[k] > P[k+1] and count == 0:
+                ST.append(k)
+        if k == P.shape[0]-1:
+            if P[k] > P[k-1] and count == 0:
+                ST.append(k)     
+    return ST
+
+
+data_1 = np.array([1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,23,3,56,3,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1])
+data = np.random.uniform(0,100, [1152])
+data_power = DCTpower.DCTpower(data)
+dataFinal = np.concatenate((data,data_1))
+print(data.shape[0])
+D = Dksparse.Dksparse(dataFinal.shape[0])
+
+st1 = STinit(dataFinal, D)
+print(st1)
+print(DCTpower.DCTpower(dataFinal[st1[-1]-5:st1[-1]]))
+print(DCTpower.DCTpower(dataFinal[st1[-1]]))
